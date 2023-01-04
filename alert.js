dd13642bbf0d2076f6748d38c91353bafbe4e777
Colin Lancaster <lancaster.colin@gmail.com>
2023-01-03 23:40:53 -0800

    whoops

--- alert.js
+++ alert.js
@@ -531,22 +531,3 @@ class Alert {
     }
 }
 
-/*
-// Test Code
- const html = `
-        <h2>Enter your search below</h2>
-        <form class="searchForm" action="{% url 'search-results' %}" method="POST">
-            <select name="entity" id="entity">
-                <option value="band">Band</option>
-                <option value="post">Post</option>
-                <option value="album">Albums</option>
-                <option value="recordLabel">Record Label</option>
-                <option value="song">Song</option>
-            </select>
-            <input class="searchInput" type="search" name="searchValue" placeholder="Search">
-            <button type="submit">Search</button>
-        </form>
-        <br/><br/>
-        `;
-Alert.CreatePrompt(html);
-*/
